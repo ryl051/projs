00b949ff89593740f188e252a87478603b1b7b38
Ryan Liu <ryanl6699@gmail.com>
2026-03-21 20:22:52 -0700

    first few ins

--- inc/chip8.h
+++ inc/chip8.h
@@ -1,11 +1,61 @@
 #include <cstdint>
 #include <random>
 
+#define START_ADDR 0x200
+#define FONTSET_SIZE 80
+#define FONTSET_START_ADDR 50
+// keypad mapping
+/*
+Keypad       Keyboard
++-+-+-+-+    +-+-+-+-+
+|1|2|3|C|    |1|2|3|4|
++-+-+-+-+    +-+-+-+-+
+|4|5|6|D|    |Q|W|E|R|
++-+-+-+-+ => +-+-+-+-+
+|7|8|9|E|    |A|S|D|F|
++-+-+-+-+    +-+-+-+-+
+|A|0|B|F|    |Z|X|C|V|
++-+-+-+-+    +-+-+-+-+
+ */
 class Chip8
 {
 private:
 	std::default_random_engine randGen;
 	std::uniform_int_distribution<uint8_t> randByte;
+	uint8_t get_Vx();
+	uint8_t get_Vy();
+	uint8_t get_byte();
+	uint8_t get_nnn();
+
+	// loading the fonts, where do we get the bytes from?
+	// i.e. for the letter 'F'
+	// 11110000
+	// 10000000
+	// 11110000
+	// 10000000
+	// 10000000
+	//
+	// notice how the 1's create the letter 'F'?
+
+	uint8_t fontset[FONTSET_SIZE] =
+	{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
 
 public: 
 	uint8_t  registers[16]{};
@@ -27,5 +77,13 @@ public:
 	void OP_1nnn();
 	void OP_2nnn();
 	void OP_3xkk();
-
+	void OP_4xkk();
+	void OP_5xy0();
+	void OP_6xkk();
+	void OP_7xkk();
+	void OP_8xy0();
+	void OP_8xy1();
+	void OP_8xy2();
+	void OP_8xy3();
+	void OP_8xy4();
 };
--- src/chip8.cpp
+++ src/chip8.cpp
@@ -5,54 +5,6 @@
 #include <chrono>
 #include <cstring>
 
-// keypad mapping
-/*
-Keypad       Keyboard
-+-+-+-+-+    +-+-+-+-+
-|1|2|3|C|    |1|2|3|4|
-+-+-+-+-+    +-+-+-+-+
-|4|5|6|D|    |Q|W|E|R|
-+-+-+-+-+ => +-+-+-+-+
-|7|8|9|E|    |A|S|D|F|
-+-+-+-+-+    +-+-+-+-+
-|A|0|B|F|    |Z|X|C|V|
-+-+-+-+-+    +-+-+-+-+
- */
-
-const uint32_t START_ADDR = 0x200;
-const uint32_t FONTSET_SIZE = 80;
-const uint32_t FONTSET_START_ADDR = 0x50;
-
-// loading the fonts, where do we get the bytes from?
-// i.e. for the letter 'F'
-// 11110000
-// 10000000
-// 11110000
-// 10000000
-// 10000000
-//
-// notice how the 1's create the letter 'F'?
-
-uint8_t fontset[FONTSET_SIZE] =
-{
-	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-	0x20, 0x60, 0x20, 0x20, 0x70, // 1
-	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-};
-
 Chip8::Chip8 () 
 	: randGen(std::chrono::system_clock::now().time_since_epoch().count()),
 	  pc(START_ADDR),
@@ -92,6 +44,23 @@ void Chip8::LoadROM(char const* filename) {
 --------------INSTRUCTIONS--------------
 */
 
+// Helpers:
+uint8_t Chip8::get_Vx() {
+	return this->opcode & (0x0F00) >> 8;
+}
+
+uint8_t Chip8::get_Vy() {
+	return (this->opcode & 0x00F0) >> 4;
+}
+
+uint8_t Chip8::get_byte() {
+	return this->opcode & 0x00FF;
+}
+
+uint8_t Chip8::get_nnn() {
+	return this->opcode & 0x0FFF;
+}
+
 // CLS
 void Chip8::OP_00E0() {
 	memset(video, 0, sizeof(video)); // note: "video" gives pointer to first element in array.
@@ -107,24 +76,20 @@ void Chip8::OP_00EE() {
 
 // Jp addr: jump to loc nnn
 void Chip8::OP_1nnn() {
-	uint16_t nnn = opcode & 0x0FFF;
-
-	pc = nnn;
+	pc = get_nnn();
 }
 
 // CALL addr: call subroutine at nnn
 void Chip8::OP_2nnn() {
-	uint16_t nnn = opcode & 0x0FFF;
-
 	stack[sp] = pc;
 	sp++;
-	pc = nnn;
+	pc = get_nnn();
 }
 
 // SE Vx, byte (kk)
 void Chip8::OP_3xkk() {
-	uint16_t kk = opcode & 0x00FF;
-	uint8_t Vx = (opcode & 0x0F00) >> 8;
+	uint16_t kk = get_byte();
+	uint8_t Vx = get_Vx();
 
 	if (registers[Vx] == kk) {
 		pc += 2;
@@ -133,8 +98,8 @@ void Chip8::OP_3xkk() {
 
 // SNE Vx, byte
 void Chip8::OP_4xkk() {
-	uint16_t kk = opcode & 0x00FF;
-	uint8_t Vx = (opcode & 0x0F00) >> 8;
+	uint16_t kk = get_byte();
+	uint8_t Vx = get_Vx();
 
 	if (registers[Vx] != kk) {
 		pc += 2;
@@ -143,8 +108,8 @@ void Chip8::OP_4xkk() {
 
 // SE Vx, Vy
 void Chip8::OP_5xy0() {
-	uint8_t Vx = (opcode & 0x0F00) >> 8;
-	uint8_t Vy = (opcode & 0x00F0) >> 4;
+	uint8_t Vx = get_Vx();
+	uint8_t Vy = get_Vy();
 
 	if (registers[Vx] == registers[Vy]) {
 		pc += 2;
@@ -153,24 +118,64 @@ void Chip8::OP_5xy0() {
 
 // LD Vx, byte
 void Chip8::OP_6xkk() {
-	uint8_t Vx = (opcode & 0x0F00) >> 8;
-	uint8_t kk = opcode & 0x00FF;
+	uint8_t Vx = get_Vx();
+	uint8_t kk = get_byte();
 
-	registers[Vx] == kk;
+	registers[Vx] = kk;
 }
 
 // ADD Vx, byte
 void Chip8::OP_7xkk() {
-	uint8_t Vx = (opcode & 0x0F00) >> 8;
-	uint8_t kk = opcde & 0x00FF;
+	uint8_t Vx = get_Vx();
+	uint8_t kk = get_byte();
 
 	registers[Vx] += kk;
 }
 
 // LD Vx, Vy
 void Chip8::OP_8xy0() {
-	uint8_t Vx = (opcode & 0x0F00) >> 8;
-	uint8_t Vy = (opcode & 0x00F0) >> 4;
+	uint8_t Vx = get_Vx();
+	uint8_t Vy = get_Vy();
 
 	registers[Vx] = registers[Vy];
+}
+
+// OR Vx, Vy
+void Chip8::OP_8xy1() {
+	uint8_t Vx = get_Vx();
+	uint8_t Vy = get_Vy();
+
+	registers[Vx] |= registers[Vy];
+}
+
+// AND Vx, Vy
+void Chip8::OP_8xy2() {
+	uint8_t Vx = get_Vx();
+	uint8_t Vy = get_Vy();
+
+	registers[Vx] &= registers[Vy];
+}
+
+// XOR Vx, Vy
+void Chip8::OP_8xy3() {
+	uint8_t Vx = get_Vx();
+	uint8_t Vy = get_Vy();
+
+	registers[Vx] ^= registers[Vy];
+}
+
+// ADD Vx, Vy
+void Chip8::OP_8xy4() {
+	uint8_t Vx = get_Vx();
+	uint8_t Vy = get_Vy();
+	uint8_t VF = 0xF;
+
+	uint16_t sum = registers[Vx] + registers[Vy];
+	if (sum > 255) {
+		registers[VF] = 1;
+	} else {
+		registers[VF] = 0;
+	}
+
+	registers[Vx] = sum & 0xFF;
 }
